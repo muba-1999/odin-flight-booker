ae09547f845b5ca8353fa232aae488b8615e1ac1
mubanga <mukobam4@gmail.com>
2022-09-21 17:58:18 +0200

    created flights and airports in the seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+airports = Airport.create([{code: "NYC"}, {code: "SFO"}])
+
+flights = Flight.create([
+    {depature_airport_id: airports.first.id, arrival_airport_id: airports.last.id, travel_date: "2022 09 18 07:00", duration: 360},
+    {depature_airport_id: airports.first.id, arrival_airport_id: airports.last.id, travel_date: "2022 09 18 10:00", duration: 360},
+    {depature_airport_id: airports.first.id, arrival_airport_id: airports.last.id, travel_date: "2022 09 18 14:00", duration: 360},
+    {depature_airport_id: airports.last.id, arrival_airport_id: airports.first.id, travel_date: "2022 09 18 07:00", duration: 380},
+    {depature_airport_id: airports.last.id, arrival_airport_id: airports.first.id, travel_date: "2022 09 18 07:00", duration: 380},
+    {depature_airport_id: airports.last.id, arrival_airport_id: airports.first.id, travel_date: "2022 09 18 07:00", duration: 380}
+])
