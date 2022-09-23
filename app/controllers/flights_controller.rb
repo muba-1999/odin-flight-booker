7f8e68195472ad334c7cbb9916a170985e681be8
mubanga <mukobam4@gmail.com>
2022-09-23 06:36:51 +0200

    added the airport variable in the index

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,6 @@
 class FlightsController < ApplicationController
+    def index
+        @airport = Airport.all.map{|a| [a.code, a.id]}
+        @travel_date = Date.civil(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+    end
 end
